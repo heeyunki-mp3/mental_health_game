edd5889ced4fee9334f69221c17b6cec0850e15c
heeyunki-mp3 <82672640+heeyunki-mp3@users.noreply.github.com>
2022-04-30 16:42:28 -0400

    questionnaire works + heidi's latest change

--- datafiles/questions.js
+++ datafiles/questions.js
@@ -4,27 +4,159 @@ questions=[
         question:"I felt lonely.", 
         choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
         answer:"",
-        reaction:["That must be nice! Hopefully, I will feel better when spring comes around too. I usually feel better when I can talk to the neighboring plants you see. Did you talk any less than usual this week?","I wonder why that might be so. Did you talk less than usual this week?"]
+        reaction:["That must be nice! Hopefully, I will feel better when spring comes around too. I usually feel better when I can talk to the neighboring plants you see. Did you talk any less than usual this week?","I wonder why that might be so. Did you talk less than usual this week?"],
+        react: [0, 1, 1, 1]
     },
     {
         number:2,
         question:"I talked less than usual.", 
         choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
         answer:"",
-        reaction:["I see. How did you feel about your interaction with other people?","I see. How did you feel about your interaction with other people?"]
+        reaction:["I see. How did you feel about your interaction with other people?"],
+        react: [0, 0, 0, 0]
     },
     {
         number:3,
         question:"I felt that people dislike me.", 
         choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
         answer:"",
-        reaction:["What did you think about the way people treated you?","What did you think about the way people treated you?"]
+        reaction:["What did you think about the way people treated you?"],
+        react: [0, 0, 0, 0]
     },
     {
         number:4,
         question:"People were unfriendly.", 
         choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
         answer:"",
-        reaction:["Well… it was nice talking to you, human. Your visit certainly brightened up my day.","Well… it was nice talking to you, human. Your visit certainly brightened up my day."]
+        reaction:["Well… it was nice talking to you, human. Your visit certainly brightened up my day."],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:5,
+        question:"I thought my life had been a failure.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["I don't think you're a failure, Moody Moon. During the night when everything is dark, you are the one who shines the brightest."],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:6,
+        question:"I felt I was just as good as other people.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["Hmmm.. I want to know more about how you feel. Last week, how often did you feel sad?"],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:7,
+        question:"I felt sad.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["Tell me more. How often did you feel depressed last week?"],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:8,
+        question:"I felt depressed", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["How often did you feel happy last week?"],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:9,
+        question:"I was happy.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["I wonder how you did around family and friends. Did you feel better around them?"],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:10,
+        question:"I felt that I could not shake off the blues even with help from my family or friends.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["Tell me more about your emotions."],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:11,
+        question:"I felt fearful.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["I see."],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:12,
+        question:"I enjoyed life.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["I see."],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:13,
+        question:"I had crying spells.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["I see. You've certainly made me day better by talking to me."],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:14,
+        question:"I feel hopeful about my future.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["Here are your stones. Goodbye friend."],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:15,
+        question:"I could not get 'going.'", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["Are you able to focus?"],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:16,
+        question:"I had trouble keeping my mind on what I was doing.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["Anything else?"],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:17,
+        question:"I was bothered by things that usually don’t bother me.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["Tell me more."],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:18,
+        question:"I felt that everything I did was an effort.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["I see."],
+        react: [0, 0, 0, 0]
+    },
+    {
+        number:19,
+        question:"My sleep was restless.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["Great Job!","I see. What about your eating?"],
+        react: [0, 1, 1, 1]
+    },
+    {
+        number:20,
+        question:"I did not feel like eating; my appetite was poor.", 
+        choices:["Rarely or none of the time (less than 1 day)","Some or a little of the time (1-2 days)", "Occasionally or a moderate amount of time (3-4 days)", "Most or all of the time (5-7 days)"],
+        answer:"",
+        reaction:["Way to go!","I see."],
+        react: [0, 1, 1, 1]
     },
 ]
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,10 @@
 function initialize(){
     //current states
-    pageIndex = 0;
+    pageIndex = 2;
     currentProgress = 0;
     showMap = false;
 
     stoneCount = 0;
-
 
     //elements from the html page
     pageTitle = document.getElementById("title");
@@ -21,7 +20,6 @@ function display(){
     if (showMap){
         displayMap();//TODO! Alayna, please implement this :DD
     }
-    
 }
 function displayBody(){
     npcDiv.innerHTML = "";
@@ -44,33 +42,64 @@ function displayBody(){
         let formEl = document.createElement("form");
         formEl.id = "questionForm";
 
-        var questionName = "question "+pageElement.dialogue.number
+        questionName = "question "+pageElement.dialogue.number
 
         //create radio button for choices for the question
-        for (var i; i<pageElement.dialogue.choices.length; i++){
+        
+        for (var i=0; i<pageElement.dialogue.choices.length; i++){
             let labelEl = document.createElement("label");
             let radioEl = document.createElement("input");
             radioEl.type = "radio";
             radioEl.name = questionName;
-            value = i;
+            radioEl.value = i;
             let choiceSpan = document.createElement("span");
             choiceSpan.innerHTML = pageElement.dialogue.choices[i];
             labelEl.appendChild(radioEl);
             labelEl.appendChild(choiceSpan);
-            formEl.appendChild(lableEl);
+            formEl.appendChild(labelEl);
         }
         
         let submitButton = document.createElement("button");
         submitButton.innerHTML = "Submit";
         submitButton.onclick = function(){
-            pageElement.dialogue.answer = formEl.questionName;
-            reactToTheResponse();
+            console.log("questionName: "+questionName);
+            choicesRadios = document.getElementsByName(questionName);
+            for (const choicesRadio of choicesRadios) {
+                if (choicesRadio.checked) {
+                    pageElement.dialogue.answer = choicesRadio.value;
+                    break;
+                }
+            }
+            //answer to the question is stored as the index of the chocies.
+            console.log(pageElement.dialogue.answer);
+            
+            //react to the answer
+            infoDiv.innerHTML=pageElement.dialogue.reaction[pageElement.dialogue.react[parseInt(pageElement.dialogue.answer)]]; 
+           
+            console.log(pageElement.nextpage);
+            //move onto the next page
+            if(!(pageElement.nextpage==null)){
+                var nextButton = document.createElement("button");
+                nextButton.innerHTML = "Next Page";
+                nextButton.onclick =function(){
+                    loadPage(pageElement.nextpage);
+                }
+                infoDiv.appendChild(nextButton);
+                //ex: nextpage = 2 (index of the page in PAGE)
+            }
+            else{
+                showMap = true;
+                displayMap();
+            }
         }
+        infoDiv.appendChild(questionEl);
+        infoDiv.appendChild(formEl);
+        infoDiv.appendChild(submitButton);
     }
     else{ // if it's an informative page (dialogue)
         var dialogueIndex = 0;
         infoDiv.innerHTML = pageElement.dialogue[dialogueIndex];
-        let nextButton = document.createElement("button");
+        var nextButton = document.createElement("button");
         nextButton.innerHTML = ">";
         nextButton.onclick =function(){
             if (dialogueIndex<pageElement.dialogue.length-1){
@@ -78,9 +107,8 @@ function displayBody(){
                 infoDiv.innerHTML = pageElement.dialogue[dialogueIndex];
             }
             else{ //finished talking (moving on to the next page)
-                if(!(pageElement.nextPage==null)){
-                    console.log("hi");
-                    loadPage(pageElement.nextPage);
+                if(!(pageElement.nextpage==null)){
+                    loadPage(pageElement.nextpage);
                     //ex: nextpage = 2 (index of the page in PAGE)
                 }
                 else{
@@ -93,9 +121,7 @@ function displayBody(){
         infoDiv.appendChild(nextButton);
     }
 }
-function reactToTheResponse(){
 
-}
 function loadPage(toPage){
     pageIndex = toPage;
     display();
